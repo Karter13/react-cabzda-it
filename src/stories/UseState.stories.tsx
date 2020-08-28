49e20a991be6d96d41a82475799de680b3b8704d
Karter13 <karmih1975@mail.ru>
2020-08-28 15:13:43 +0300

    made React kabzda №19

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,16 +1,26 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 
 export default {
     title: 'UseState demo'
 }
 
+function denerateData() {
+    //difficult counting
+    console.log('denerateData');
+    return 1;
+}
+
 export const Example1 = () => {
     console.log('example1');
-    const [counter, setCounter] = useState<number>(0);
+
+    // const initvalue = useMemo(denerateData, []);
+
+    //useState умеет без useMemo
+    const [counter, setCounter] = useState<number>(denerateData);
 
 
     return <>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(state => state + 1)}>+</button>
         {counter}
     </>
 };
