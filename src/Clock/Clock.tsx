221980ee8c51acecb4f7e6afb92b4785574ae764
Karter13 <KarMiH1975@mail.ru>
2020-09-03 22:46:09 +0300

    wrote the functionality for component Clock

--- src/Clock/Clock.tsx
+++ src/Clock/Clock.tsx
@@ -1,12 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type PropsType = {
 
 }
 
-export const Clock: React.FC<PropsType> = (props) => {
-    const[date, setDate] = useState(new Date())
-    return <div>
+const getTwoDigitString = (num: any) => num < 10 ? '0' + num : num;
 
+export const Clock: React.FC<PropsType> = (props) => {
+    const[date, setDate] = useState(new Date());
+
+    useEffect(() => {
+
+        const intervalId = setInterval(() => {
+            console.log('TICK');
+            setDate(new Date())
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+
+    }, []);
+
+    return <div>
+        <span>{getTwoDigitString(date.getHours())}</span>
+        :
+        <span>{getTwoDigitString(date.getMinutes())}</span>
+        :
+        <span>{getTwoDigitString(date.getSeconds())}</span>
     </div>
 };
